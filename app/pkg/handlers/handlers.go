c0acc618071340aea997329780b29f278f7f0489
INegmetov <negmetov.i@gmail.com>
2024-01-05 16:31:15 +0400

    handling json marshal

--- app/pkg/handlers/handlers.go
+++ app/pkg/handlers/handlers.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/inegmetov/bookings/pkg/config"
 	"github.com/inegmetov/bookings/pkg/models"
 	"github.com/inegmetov/bookings/pkg/render"
+	"log"
 	"net/http"
 	_ "net/http/pprof"
 )
@@ -31,7 +34,7 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
-	render.RenderTemplate(w, "home.gohtml", &models.TemplateData{})
+	render.RenderTemplate(w, r, "home.gohtml", &models.TemplateData{})
 }
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// perform some logic
@@ -42,30 +45,56 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 	stringMap["remote_ip"] = remoteIP
 
-	render.RenderTemplate(w, "about.gohtml", &models.TemplateData{
+	render.RenderTemplate(w, r, "about.gohtml", &models.TemplateData{
 		StringMap: stringMap,
 	})
 }
 
 // Reservation renders the make a reservation page and display form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "make-reservation.gohtml", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.gohtml", &models.TemplateData{})
 }
 
 // Generals renders the room page
 func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "generals.gohtml", &models.TemplateData{})
+	render.RenderTemplate(w, r, "generals.gohtml", &models.TemplateData{})
 }
 
 // Majors renders the room page
 func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "majors.gohtml", &models.TemplateData{})
+	render.RenderTemplate(w, r, "majors.gohtml", &models.TemplateData{})
 }
 
 func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "search-availability.gohtml", &models.TemplateData{})
+	render.RenderTemplate(w, r, "search-availability.gohtml", &models.TemplateData{})
+}
+
+func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start := r.Form.Get("start")
+	end := r.Form.Get("end")
+
+	w.Write([]byte(fmt.Sprintf("start date is %s and end date is %s", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      true,
+		Message: "Available",
+	}
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "contact.gohtml", &models.TemplateData{})
+	render.RenderTemplate(w, r, "contact.gohtml", &models.TemplateData{})
 }
